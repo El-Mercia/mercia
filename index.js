9aee4e6bee4d4fa712ec4525158de4e6ecf34642
Mercia <merciarandrianome@gmail.com>
2020-03-18 16:39:10 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+console.log("hello world !");
+
+let nbOfScreenColumns = 2;
+
+nbOfScreenColumns = "two";
+
+const nbOfTry = 3;
+
+// On ne réaffecte pas une variable constante (cette ligne va lamentablement échouer ... ): 
+//variable3 = "vais je réussir à changer";
+
+/*
+ce commentaire ne sera pas éxecuter, c'est juste pour marqué un long commentaire
+qui s'execute en première ligne
+afin de comprendre comment ca marche son IDE
+*/
+
+console.log("variable 2 : ", nbOfScreenColumns);
+console.log("variable 3 ", nbOfTry);
+
+// TYPES OF VARIABLES
+
+const strVariable = "chaine de carasctère '''''''";
+const numberVariable = 3.14;
+const booleanVariable = true;
+const arrayaVariable = [
+"cookies",
+3310,
+true,
+[]
+];
+const personalInfos = {
+	favoriteBiscuit: "cookies",
+	favoriteNokia: 3310,
+	haveGoodJokes: true,
+	friends: [],
+}
+
+//console.log("favoriteNokia", personalInfos.favoriteNokia);
+
+/*const Students = [
+{name: "mercia", age: 30},
+{name: "marie", age: 40},
+{name: "bob", age: NaN},
+]
+
+const result = 3 / "chaine de caractère";
+
+console.log(result);
+*/
+
